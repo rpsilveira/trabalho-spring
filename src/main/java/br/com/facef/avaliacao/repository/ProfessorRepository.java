aa156f566d351332cfdf1dc4cd3ecb7f394ec8be
REINALDO DE PAULA SILVEIRA <22323@unifacef.edu.br>
2019-10-19 10:37:39 -0300

    adicionada anotação Repository na classe Professor; alterada a engine do banco para InnoDB

--- src/main/java/br/com/facef/avaliacao/repository/ProfessorRepository.java
+++ src/main/java/br/com/facef/avaliacao/repository/ProfessorRepository.java
@@ -2,7 +2,9 @@ package br.com.facef.avaliacao.repository;
 
 import br.com.facef.avaliacao.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
 
 }
